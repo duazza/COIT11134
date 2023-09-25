cdd006e56fc51407c55fa3135d5146a7c31c65f1
duazza <141012436+duazza@users.noreply.github.com>
2023-09-26 01:35:24 +1000

    test26.9

---
+++ 25.9ProjectTest/src/main/java/com/cqumail/powell/duane/coit11134y23y2/projecttest/TestClass.java
@@ -0,0 +1,18 @@
+package com.cqumail.powell.duane.coit11134y23y2.projecttest;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author duane
+ */
+public class TestClass {
+    
+    
+public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
