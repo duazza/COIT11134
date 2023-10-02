be481ca4b56d1faccc2f14ed41d0a04c4963444f
duazza <141012436+duazza@users.noreply.github.com>
2023-10-02 16:44:30 +1000

    2/10 stockTake now working

---
+++ newTest/src/main/java/tropcioassessment2/newtest/NewTest.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package tropcioassessment2.newtest;
+
+/**
+ *
+ * @author duane
+ */
+public class NewTest {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
